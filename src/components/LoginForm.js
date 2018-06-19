1573460f5f53db811da8e39859a656844c827bc1
Anuj Kumar <anujkuma@adobe.com>
2018-06-19 14:19:18 +0530

    02_Init

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { Card, CardSection, ButtonComponent } from './common';
+import { Card, CardSection, ButtonComponent, Input } from './common';
 
 class LoginForm extends Component {
+  state = { email: '', password: '' };
+
   render() {
     return (
       <Card>
-        <CardSection />
-        <CardSection />
+        <CardSection>
+          <Input
+            placeholder="user@gmil.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
         <CardSection>
           <ButtonComponent
             text="Log In"
